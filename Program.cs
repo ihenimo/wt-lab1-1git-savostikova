1b7fc27ad24f17f9b6c3e0f7220e5dc4a71ec396
Ангелина Савостикова <6790397sa@gmail.com>
2026-09-20 19:53:30 +0300

    feat: add logic to Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Лабораторная работа №1 по Git
+// Ветка: feature/vs-net-logic
+
+Console.WriteLine("Моё лето 2026!");
+Console.WriteLine("Работаю в Visual Studio 2022");
+Console.WriteLine("Ветка: feature/vs-net-logic");
+
+// Подключённые пакеты:
+// - Npgsql.EntityFrameworkCore.PostgreSQL
+// - Newtonsoft.Json
+
+Console.WriteLine("Готово!");
